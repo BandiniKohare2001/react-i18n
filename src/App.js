3216a2c48106de51c2e4d84176f70b7ebc5a5f1a
Bandini2001 <bandinikohare30@gmail.com>
2023-11-22 17:54:06 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import I18n from "./utils/i18n"
 import './App.css';
 import React, { useState } from "react";
 
+import poster2 from "./poster2.webp"
+
 function App() {
   const usersCount = 15;
   // const [ lang, setLang] = useState("hi");
   return (
 
    <div className="container">
+    <img src={poster2} from alt="poster2" className="img2" />
+  
     <h1 className="welcome">{I18n("welcomeMessage")}</h1>
+    <h1 className="i18">I18n</h1>
     <p> {I18n("normalMessage")}
     </p>
     <select
@@ -22,10 +27,11 @@ function App() {
                 <option value="hi">Hindi</option><hr />
                 <option value="mr">Marathi</option>
             </select>
-    <h3>{I18n("greetingMessage")}</h3>
+    <p>{I18n("greetingMessage")}</p>
+    <p>{I18n("greetingMessage2")}</p>
 
     {/* <p> {usersCount} users are learning in this session. </p> */}
-    <p>{I18n("userStatMessage", "<studentCount>", usersCount)}</p>
+    {/* <p>{I18n("userStatMessage", "<studentCount>", usersCount)}</p> */}
     </div>
   );
 }
