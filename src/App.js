dc595a03b74da0b7735f547cdb01ca1863e27219
Bandini2001 <bandinikohare30@gmail.com>
2023-11-22 13:06:02 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <div>
+    <h1>Welcome</h1>
+    <p>
+      We hope you enjoy learning React.
+    </p>
+
+    <h3> Have a grate day</h3>
+    </div></>
+  );
+}
+
+export default App;
